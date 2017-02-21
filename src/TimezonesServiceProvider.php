c54726bfaaf64993f5757cbd1c98e0831e9fba04
TranNguyen <nguyentc@runsystem.net>
2017-02-21 14:18:14 +0700

    [add] first packages

--- src/TimezonesServiceProvider.php
+++ src/TimezonesServiceProvider.php
@@ -14,10 +14,10 @@ class TimezonesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $this->loadViewsFrom(__DIR__.'/views', 'timezones');
-        $this->publishes([
-            __DIR__.'/views' => base_path('resources/views/trannguyen/timezones'),
-        ]);
+        $this->loadViewsFrom(__DIR__.'/views', 'timezones');
+//        $this->publishes([
+//            __DIR__.'/views' => base_path('resources/views/trannguyen/timezones'),
+//        ]);
     }
 
     /**
